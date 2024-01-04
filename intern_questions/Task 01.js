c56bfd73790c0ee001b8dc52ab0a0ed1f43a107e
Aryantiwarib <143889805+Aryantiwarib@users.noreply.github.com>
2024-01-04 19:27:09 +0530

    Add files via upload

---
+++ intern_questions/Task 01.js
@@ -0,0 +1,8 @@
+container.addEventListener('mouseout',function run(){ 
+    document.getElementById('container').style.background="rgb(20 24 19)";
+    document.getElementById('container').style.fontFamily="cursive";
+    document.getElementById('one').style.color="#f3593d";
+    document.getElementById('two').style.color="#f3593d";
+    document.getElementById('three').style.color="#f3593d";
+    document.getElementById('four').style.color="#f3593d";
+}); 
