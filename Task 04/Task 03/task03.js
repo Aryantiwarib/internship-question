3dfba1d2fb8630a3d2d15c6d35b4df54be016f49
Aryantiwarib <143889805+Aryantiwarib@users.noreply.github.com>
2024-02-05 21:04:14 +0530

    Add files via upload

---
+++ Task 04/Task 03/task03.js
@@ -0,0 +1,10 @@
+function ram(){
+    let button=document.querySelector('button');
+    let id=button.getAttribute('id');
+    // var bu1=document.getElementById('button1').value;
+    // var bu2=document.getElementById('p');
+    // var bu2=document.getA('p');
+    // document.getElementById('button1').innerHTML="X";
+    console.log(id);
+    // console.log();
+}
